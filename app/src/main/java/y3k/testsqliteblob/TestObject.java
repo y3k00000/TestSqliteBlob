f9c08fb18eac073256a048af00c718216cb905a0
y3k00000 <y3k00000@github.com>
2017-07-21 09:27:31 +0800

    Add Comments
    test

--- app/src/main/java/y3k/testsqliteblob/TestObject.java
+++ app/src/main/java/y3k/testsqliteblob/TestObject.java
@@ -2,6 +2,9 @@ package y3k.testsqliteblob;
 
 import java.io.Serializable;
 
+/**
+ * Serializable Object For tTest.
+ */
 public class TestObject implements Serializable {
     public final int id;
     public final String name;
